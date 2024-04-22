361c31122cbd63062adf0f8d9262bca311fb7e8f
YukiAyuta <22fi006@ms.dendai.ac.jp>
2024-04-22 12:24:33 +0900

    CSVReaderExample

--- java-basic-1/src/HowOldAreYou.java
+++ java-basic-1/src/HowOldAreYou.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 
 public class HowOldAreYou {
 
-	public static void main(String[] args) { 
+	public static void main(String[] args) {
 
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			// BufferedReader というのは、データ読み込みのクラス(型)
@@ -19,7 +19,22 @@ public class HowOldAreYou {
 			int age = Integer.parseInt(line);
 			System.out.println("あなたは" + age + "歳ですね。");
 			System.out.println("あなたは2030年に、" + (age + 6) + "歳ですね。");
-			System.out.println("あなたが生まれた元号は、" + (age + 6) + "歳ですね。");
+			if(0 <= age && age <= 5){
+				System.out.println("あなたが生まれた元号は、令和ですね。");
+			}else if(5 < age && age <= 35){
+				System.out.println("あなたが生まれた元号は、平成ですね。");
+			}else if(35 < age && age <= 98){
+				System.out.println("あなたが生まれた元号は、昭和ですね。");
+			}else if(98 < age && age <= 112){
+				System.out.println("あなたが生まれた元号は、大正ですね。");
+			}else if(112 < age && age <= 120){
+				System.out.println("あなたが生まれた元号は、明治ですね。");
+			}else if(120 < age || age < 0){
+				System.out.println("再入力してください。");
+				line = reader.readLine();
+				age = Integer.parseInt(line);
+			}
+		
 		}
 		catch(IOException e) {
 			System.out.println(e);
