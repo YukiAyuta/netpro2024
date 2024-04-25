f2ed327078c7331d1718865670c9d549c5e52f29
YukiAyuta <22fi006@ms.dendai.ac.jp>
2024-04-25 12:18:51 +0900

    Kadai2

--- java-basic-1/src/Kadai2HowOldAreYou3.java
+++ java-basic-1/src/Kadai2HowOldAreYou3.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class HowOldAreYou {
+public class Kadai2HowOldAreYou3 {
 
 	public static void main(String[] args) {
 
