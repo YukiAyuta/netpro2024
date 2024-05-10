b87bcc9400eb178f7ce45574f937d45129de232a
YukiAyuta <22fi006@ms.dendai.ac.jp>
2024-05-10 11:01:38 +0900

    tuika

--- java-basic-2/src/HeikinCKadai.java
+++ java-basic-2/src/HeikinCKadai.java
@@ -1,5 +1,7 @@
 package src;
+
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class HeikinCKadai {
 	public static void main(String[] args){
@@ -7,17 +9,23 @@ public class HeikinCKadai {
 int h = 0;
 int sum = 0;
 int[] tensu = new int [100];
-ArrayList<Integer>highscire = new ArrayList<>();
+ArrayList<Integer>s = new ArrayList<>();
 for(int i = 0; i <= 100; i ++){
 	Kamoku math = new Kamoku(random.next(bound:101));
 	tensu[i] = math.getScore();
 	sum += tensu[i];
-}
-	Kamoku math = new Kamoku(70);
 
-	System.out.println("数学の点は" + b + "ですね");
+	if(tensu[i] >= 80){
+		s.add(tensu[i]);
+	}
+}
 		int ave=(sum)/tensu.length;
-		System.out.println("平均点は" + ave + "ですね");
+		System.out.println("平均点は" + ave + "です。");
+		System.out.println("合格者は以下の通りです。");
+		Collections.sort(s);
+		for(int i:s){
+			System.out.println(i);
+		}
 
 
 	}
