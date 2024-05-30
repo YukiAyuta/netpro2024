68d75186368989aa442bdfe1105c71d295b28ba4
YukiAyuta <22fi006@ms.dendai.ac.jp>
2024-05-30 22:26:17 +0900

    mikannsei

---
+++ socket-3/src/basic/ExThreadsMain.java
@@ -0,0 +1,60 @@
+package src.basic;
+
+// Runnable インターフェースを実装することで、このクラスのインスタンスはスレッドとして実行可能になります。
+public class ExThreadsMain implements Runnable {
+
+    // main メソッドはプログラムのエントリーポイントです。
+    public static void main(String[] args) {
+        // 2つの文字を初期化します。
+        char c1 = 97; // ASCII値 97 は 'a' です
+        char c2 = (char) (c1 + 1); // c1 に 1 を足すと ASCII値 98 になり、'b' になります
+
+        // 初期化した文字をコンソールに出力します。
+        System.out.println(c1); // 出力: a
+        System.out.println(c2); // 出力: b
+
+        // ExThreadsMain クラスのインスタンスを作成します。
+        ExThreadsMain ct1 = new ExThreadsMain(); // CountTenBクラスのインスタンスとして
+        ExThreadsMain ct2 = new ExThreadsMain(); // CountTenBクラスのインスタンスとして
+
+        ct1.setChar(c1);
+        ct2.setChar(c2);
+
+        // ct を実行する新しいスレッドを作成します。
+        Thread th1 = new Thread(ct1, "th-1");// th-1はスレッド名
+        Thread th2 = new Thread(ct2, "th-2");
+
+        // スレッドを開始します。これにより、ExThreadsMain の run メソッドが呼び出されます。
+        th1.start();
+        th2.start();
+
+        // この try-catch ブロックは、0 から 9 までの値を 500 ミリ秒間隔で出力するループを実行します。
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("main:i=" + i);
+
+                // メインスレッドを 500 ミリ秒間一時停止します。
+                Thread.sleep(500); // ミリ秒単位のスリープ時間
+            }
+        } catch (InterruptedException e) {
+            // スレッドが中断された場合は、例外を出力します。
+            System.err.println(e);
+        }
+    }
+
+    // run メソッドは、新しいスレッドが実行するコードを含みます。
+    public void run() {
+        // この try-catch ブロックは、0 から 9 までの値を 1000 ミリ秒間隔で出力するループを実行します。
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("runnable thread:i=" + i);
+
+                // スレッドを 1000 ミリ秒間一時停止します。
+                Thread.sleep(1000); // ミリ秒単位のスリープ時間
+            }
+        } catch (InterruptedException e) {
+            // スレッドが中断された場合は、例外を出力します。
+            System.err.println(e);
+        }
+    }
+}
---
+++ socket-3/src/basic/packagerei.java
@@ -0,0 +1,25 @@
+package src.basic;
+
+public class packagerei {
+    // s1 = new String("ねこ");
+    
+    // String s2 = new String("ねこ");
+
+    s1 = "ねこ";
+
+    String s2 = "ねこ";
+
+    if(s1==s2)
+    {// 中の文字列をみて判定してくれている。
+
+        System.out.println("yes! s1==s2:" + s1 + ":" + s2);
+
+    }else
+    {
+
+        System.out.println("no! s1=s2" + s1 + ":" + s2); // new Stringしたときだけobjectとしてふるまい判定にも
+
+        // オブジェクトハッシュIDで比較させる。なのでその場合はequals()で比較しよう。
+
+    }
+}
