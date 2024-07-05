0b7ea395bb0331779c966dcf797c6d908698aee0
YukiAyuta <22fi006@ms.dendai.ac.jp>
2024-07-05 10:40:22 +0900

    iiiii

--- mid_exam/src/final_exam/quiz.java
+++ mid_exam/src/final_exam/quiz.java
@@ -1,47 +1,56 @@
 package final_exam;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class quiz {
     String ans;
-    String cor;
-    String q;
-    String[] correct = { "A" };
-    String[] quiz = { "Aは何でしょう？" };
+    String[] q;
+    String[] c;
+    List<Integer> targetList = new ArrayList<>(Arrays.asList(0, 1, 2));
+    String[] quiz = { "Aは何でしょう？" , "Bはどこでしょう？", "Cは何語でしょう？"};
+    String[] correct = { "A", "B", "C" };
 
-    // public void qanda() {
-    // int n = (int) Math.random();
-    // cor = correct[n];
-    // }
+    public void setQandCor() {
+        Collections.shuffle(targetList);
+        for (int i = 0; i < quiz.length; i++) {
+            q[i] = quiz[targetList.get(i)];
+            c[i] = correct[targetList.get(i)];
+        }
+    }
 
     public void setAns(String ans) {
         this.ans = ans;
     }
 
-    public void setCor() {
-        this.cor = correct[0];
-    }
+    // public void setCor(String cor) {
+    // this.cor = correct[0];
+    // }
 
-    public void setQuiz() {
-        this.q = quiz[0];
-    }
+    // public void setQuiz(String q) {
+    // this.q = quiz[0];
+    // }
 
-    public void matchingAns() {
-        if (ans == cor) {
-            System.out.println("Correct!");
-        } else {
-            System.out.println("Again...");
-        }
-    }
+    // public void matchingAns() {
+    // if (ans == cor) {
+    // System.out.println("Correct!");
+    // } else {
+    // System.out.println("Again...");
+    // }
+    // }
 
     public String getAns() {
         return ans;
     }
 
-    public String getCor() {
-        return cor;
+    public String getCor(int n) {
+        return c[n];
     }
 
-    public String getQuiz() {
-        return q;
+    public String getQuiz(int n) {
+        return q[n];
     }
 
 }
